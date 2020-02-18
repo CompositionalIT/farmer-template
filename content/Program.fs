e171e23b7a3d46825ac65ad8022909aee19529e3
isaacabraham <isaac.abraham@gmail.com>
2020-02-18 20:13:43 +0000

    Update program.fs

--- content/Program.fs
+++ content/Program.fs
@@ -3,13 +3,15 @@ open Farmer.Resources
 open Farmer.Deploy
 open System
 
-// Create ARM resources here e.g. webApp { } or storage { } etc.
+// Create ARM resources here e.g. webApp { } or storageAccount { } etc.
+// See https://compositionalit.github.io/farmer/api-overview/resources/ for more details.
 
-// Add the resources to the deployment using the add_resource keyword. 
+// Add resources to the ARM deployment using the add_resource keyword.
+// See https://compositionalit.github.io/farmer/api-overview/resources/arm/ for more details.
 let deployment = arm {
     location NorthEurope
 }
 
-let output =
-    deployment
-    |> Writer.quickWrite "output"
+printf "Generating ARM template..."
+deployment |> Writer.quickWrite "output"
+printfn "all done! Template written to output.json"
