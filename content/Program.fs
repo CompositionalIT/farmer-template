047b91e9faae7ce22365e459f8c5e33cca90a2ee
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-26 18:31:57 +0200

    Updates and simplifications to template

--- content/Program.fs
+++ content/Program.fs
@@ -9,9 +9,14 @@ open System
 // Add resources to the ARM deployment using the add_resource keyword.
 // See https://compositionalit.github.io/farmer/api-overview/resources/arm/ for more details.
 let deployment = arm {
-    location Location.NorthEurope
+    location Location.WestEurope
 }
 
 printf "Generating ARM template..."
 deployment |> Writer.quickWrite "output"
 printfn "all done! Template written to output.json"
+
+// Alternatively, deploy your resource group directly to Azure here.
+// deployment
+// |> Deploy.execute "farmer-resource-group" Deploy.NoParameters
+// |> printfn "%A"
--- content/ProgramDeploy.fs
+++ content/ProgramDeploy.fs
@@ -1,6 +1,5 @@
 open Farmer
-open Farmer.Resources
-open Farmer.Deploy
+open Farmer.Builders
 open System
 
 // Create ARM resources here e.g. webApp { } or storageAccount { } etc.
@@ -9,30 +8,22 @@ open System
 // Add resources to the ARM deployment using the add_resource keyword.
 // See https://compositionalit.github.io/farmer/api-overview/resources/arm/ for more details.
 let deployment = arm {
-    location NorthEurope
+    location Location.WestEurope
 }
 
 module Config =
     let private getEnv name =
         match Environment.GetEnvironmentVariable name with
-        | null -> failwithf "Missing environment variable %s" name
-        | name -> name
-    let private getEnvGuid = getEnv >> Guid
-    let credentials =
-        { ClientId = getEnvGuid "CLIENT_ID"
-          ClientSecret = getEnvGuid "CLIENT_SECRET"
-          TenantId = getEnvGuid "TENANT_ID" }
-    let subscriptionId = getEnvGuid "SUBSCRIPTION_ID"
-    let resourceGroupName = getEnv "RESOURCE_GROUP_NAME"
+        | null -> None
+        | name -> Some name
+    let resourceGroupName =
+        getEnv "RESOURCE_GROUP_NAME" |> Option.defaultValue "farmer-ci-deploy"
 
 let response =
     deployment
-    |> fullDeploy Config.credentials Config.subscriptionId Config.resourceGroupName []
-
-let description =
-    match response.Result with
+    |> Deploy.tryExecute Config.resourceGroupName Deploy.NoParameters
+    |> function
     | Ok outputs -> sprintf "Success! Outputs: %A" outputs
-    | Error (DeploymentRejected error) -> sprintf "Rejected! %A" error
-    | Error (DeploymentFailed error) -> sprintf "Failed! %A" error
+    | Error error -> sprintf "Rejected! %A" error
 
-printfn "Deployment %s finished with result: %s" response.DeploymentName description
+printfn "Deployment finished with result: %s" response
